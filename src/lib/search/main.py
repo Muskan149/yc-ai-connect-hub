7dc1e3b9262bfcaa5814f051ddf0e3b5884c8302
Muskan Mahajan <58877651+Muskan149@users.noreply.github.com>
2025-06-15 11:30:00 -0700

    changed port

--- src/lib/search/main.py
+++ src/lib/search/main.py
@@ -52,5 +52,5 @@ async def perform_index_profile(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
